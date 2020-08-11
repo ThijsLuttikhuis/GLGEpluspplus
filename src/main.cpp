9e35b3b088844d956f9bc8e8005fab6d8092ec65
thijs <t.luttikhuis@student.utwente.nl>
2020-08-11 21:48:22 +0200

    Create window

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,71 @@
 // Created by thijs on 11-08-20.
 //
 
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
 int main() {
+    GLFWwindow* window;
+
+    // Initialize glfw
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open window
+    window = glfwCreateWindow(1024, 768, "Window", nullptr, nullptr);
+    if (window == nullptr) {
+        fprintf(stderr,
+                "Failed to open GLFW window.");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    // Capture keys
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Dark grey background
+    glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
+
+    while (true) {
+        // Clear screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        // Check for exit button
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS ||
+            glfwWindowShouldClose(window) != 0) {
+            break;
+        }
+    }
+
+    // Exit procedure
+    glfwTerminate();
 
     return 0;
 
